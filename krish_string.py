7f086afb8b850945502fe31d7eee85dc56b50574
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2023-09-16 21:13:28 +0530

    Update and rename hell_string.py to krish_string.py

--- krish_string.py
+++ krish_string.py
@@ -33,8 +33,8 @@ except:
 
 
 def main():
-    print("T E A M    H E L L B O T   ! !")
-    print("Hello!! Welcome to HellBot Session Generator\n")
+    print("T E A M   KRISH B O T   ! !")
+    print("Hello!! Welcome to KrishBot Session Generator\n")
     print("Human Verification Required !!")
     while True:
         verify = int(randint(1, 50))
@@ -62,18 +62,18 @@ def main():
             print("Verification Failed! Try Again:")
 
 
-def generate_hellbot_session():
-    print("!!! HELLBOT SESSION !!!")
-    print("One session for all HellBot's Project.")
+def generate_krishbot_session():
+    print("!!! KRISHBOT SESSION !!!")
+    print("One session for all KrishBot's Project.")
     api_id = int(input("\nEnter APP ID here: "))
     api_hash = input("\nEnter API_HASH here: ")
-    with PClient(name="helluser", api_id=api_id, api_hash=api_hash, in_memory=True) as hell:
-        print("\nYour HELLBOT SESSION is saved in your telegram saved messages.")
-        _session = hell.export_session_string()
-        hell_session = hellbot_session(_session)
-        hell.send_message(
+    with PClient(name="krishuser", api_id=api_id, api_hash=api_hash, in_memory=True) as krish:
+        print("\nYour KRISHBOT SESSION is saved in your telegram saved messages.")
+        _session = krish.export_session_string()
+        krish_session = krishbot_session(_session)
+        krish.send_message(
             "me",
-            f"#HELLBOT_SESSION \n\n`{hell_session}`",
+            f"#KRISHBOT_SESSION \n\n`{krish_session}`",
         )
 
 
@@ -81,28 +81,28 @@ def generate_pyro_session():
     print("Pyrogram Session for Music Bot!")
     APP_ID = int(input("\nEnter APP ID here: "))
     API_HASH = input("\nEnter API HASH here: ")
-    with PClient(name="helluser", api_id=APP_ID, api_hash=API_HASH, in_memory=True) as hellbot:
-        print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
-        hellbot.send_message(
+    with PClient(name="krishuser", api_id=APP_ID, api_hash=API_HASH, in_memory=True) as krishbot:
+        print("\nYour KrishBot Session Is sent in your Telegram Saved Messages.")
+        krishbot.send_message(
             "me",
-            f"#HELLBOT #PYROGRAM\n\n`{hellbot.export_session_string()}`",
+            f"#KRISHBOT #PYROGRAM\n\n`{krishbot.export_session_string()}`",
         )
 
 
 def generate_telethon_session():
-    print("\nTelethon Session For HellBot!")
+    print("\nTelethon Session For KrishBot!")
     APP_ID = int(input("\nEnter APP ID here: "))
     API_HASH = input("\nEnter API HASH here: ")
-    with TelegramClient(StringSession(), APP_ID, API_HASH) as hellbot:
-        print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
-        hellbot.send_message(
+    with TelegramClient(StringSession(), APP_ID, API_HASH) as krishbot:
+        print("\nYour KrishBot Session Is sent in your Telegram Saved Messages.")
+        krishbot.send_message(
             "me",
-            f"#HELLBOT #TELETHON \n\n`{hellbot.session.save()}`",
+            f"#KRISHBOT #TELETHON \n\n`{krishbot.session.save()}`",
         )
 
 
 def generate_insta_session():
-    print("Instagram Session For HellBot!")
+    print("Instagram Session For KrishBot!")
     cl = IClient()
     username = input("Enter your Instagram Username: ")
     password = input("Enter your Instagram Password: ")
@@ -126,7 +126,7 @@ def challenge_code(username, choice):
     return otp
 
 
-def hellbot(text):
+def krishbot(text):
     res = ''.join(
         map(
             random.choice,
@@ -136,7 +136,7 @@ def hellbot(text):
     return res.strip()
 
 
-def hellbot_session(session):
+def krishbot_session(session):
     pyro_format = {
         351: ">B?256sI?",
         356: ">B?256sQ?",
@@ -151,7 +151,7 @@ def hellbot_session(session):
         5: "91.108.56.130",
     }
 
-    error_msg = "Error in generating session! Report it in Hell Chats"
+    error_msg = "Error in generating session! Report it in Krish Chats"
 
     # converting pyrogram session
     if len(session) in pyro_format.keys():
@@ -176,7 +176,7 @@ def hellbot_session(session):
                 auth_key
             )
         )
-        return f"=={hellbot('hell')}{new_session}{hellbot('bot')}=="
+        return f"=={krishbot('krish')}{new_session}{krishbot('bot')}=="
     else:
         return error_msg
 
